24f5f5c453aec812299d75151e5e546028d33a12
Your Name <your@email.com>
2026-07-12 17:35:01 -0400

    redid merge sort

--- main.py
+++ main.py
@@ -85,12 +85,91 @@ def factorial(n):
         return 1
     else:
         return n*(factorial(n-1))
-print(factorial(4))
-        
 
 
+def sumlst(arr):
+    #base case-- returns that arrays element at last index
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        return arr[0] + sumlst(arr[1:])
+
+def reverse(s):
+    #base case is when the length is equal to 1, you are on the last letter
+    if len(s) == 1:
+        return s
+    else:
+        return s[-1] + reverse(s[:-1])
+def power(base, exp):
+    #base is when the exp is equal to 1 since it is just the base returned, then we multiply everything by that
+    if exp == 1:
+        return base
+    else:
+        return base * power(base, (exp-1))
+def sum_dig(n):
+    string = str(n)
+    if len(string) == 1:
+        return int(string)
+    else:
+        return int(string[0]) + sum_dig(string[1:])
+def palindrome(s):
+    if s == '':
+        return True
+    elif s[0] != s[-1]:
+        return False
+    else:
+        return palindrome(s[1:-1])
+
+def fib(n):
+    ##double base
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 
+##merge sort
+def mergesort(arr):
+    #base case checks if the length is 1
+    #this will return that single element array to whichever side variable called it in a recursive function
+    if len(arr) <= 1:
+        return arr
+    else:
+        #split the array in half--this will happen logn times
+        middle = len(arr) //2
+        #create the left/right sorted arrs, these will call mergesort to further divide
+        #each call will return a merged arr to the variable calling it
+        left_sorted = mergesort(arr[:middle])
+        right_sorted = mergesort(arr[middle:])
+
+        #return a single merged array in order from smalles to largest
+        return merge(left_sorted, right_sorted)
+##merge helper function-- merges together the 2 arrays by comparing pointers
+def merge(left, right):
+    #create indexes for the pointers which will be incremented each time one is added to result
+    j,i = 0,0
+    result = []
+    #loop until one is empty
+    while (i < len(right)) and (j < len(left)):
+        if left[j] < right[i]:
+            result.append(left[j])
+            j+=1
+        else:
+            result.append(right[i])
+            i+=1
+    #if empty dump onto the list since the rest is sorted from prev calls
+    if left:
+        result.extend(left[j:])
+    else:
+        result.extend(right[i:])
+    #return the merged array in sorted order
+    return result
+
+
+    
+    
 
 
 
