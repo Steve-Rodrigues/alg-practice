0ea28c86373b514f39f597ebd8f37d0cb9c86109
= <stevie.rodrigues27@gmail.com>
2026-08-01 15:42:56 -0400

    Implemented kruskals algorithm for finidng a minimum spanning tree from a graph. It takes in a graph whihc is an array of edges and then from that array it will try to make a result tree which is going to show how all of the vertices would point at each other in the smallest cost possible based on the edge wiehgts from the graph. If it is possible for all of the vertices to be connected than the result tree array will show everyhtinbg as one value since they are pointing to the same array (node) spot. We also return the total weight since it was minimized, and also a root in the result tree is defined as an index whose value is its own index.

--- mst/spanning_trees.py
+++ mst/spanning_trees.py
@@ -8,25 +8,127 @@
 #so the data structure allows us to union two graphs into the same graph which is now connected but conaining no cycles
 class DisjointSet:
     #each node will have a parent pointer to check set memebership and a value, also neighbor value to attach to another node
-    def __init__(self, parent, val, next=None):
+    def __init__(self, parent, val):
         self.parent = parent
-        self.val = val, self.next = next
+        self.val = val
     #find operation to check set membership-- gets the value of the current nodes parent, if same as node comparing with then in the same tree
     #get the parent by checking nodes neighbor until the parent value is that nodes value
-    def find(node):
-        currNode = node
-        while currNode.parent.val != currNode.val:
-            currNode = currNode.next
-        return currNode.val
+    def find(self, node):
+        #first pass is to get the root node of the node
+        root = node
+        while root.parent is not root:
+            root = root.parent
+        #go throug all nodes again setting their parent directly to the root(compression)
+        while node.parent is not node:
+            curr_parent = node.parent
+            node.parent = root
+            node = curr_parent
+        return root
+
+
     #union will add an edge between two nodes but will make the parent equal to the same node
     #only runs if the result of find is a different parent node(they are not connected yet)
     def union(self, n1, n2):
-        n1Parent = self.find(n1)
-        n2Parent = self.find(n2)
+        root1 = self.find(n1)
+        root2 = self.find(n2)
         #going to point node 2 parent to the parent of node 1 before adding edge between node 1 and 2
-        if n1Parent != n2Parent:
-            n2.parent = n1.parent
-            n2.next = n1
+        if root1 is not root2:
+            root2.parent = root1
         return
     
+#this graph class will hold everything for kruskals algorithm
+#starts with a normal graph made of an array edged which are undirected(both ways) and a weight
+#we take in num of vertices so we can create the parent array tree
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = []#going to have (weight, v1, v2) for the edges
+    #method for adding an edge to the graph an it is undirectiounal
+    def addEdge(self, v1, v2, w):
+        self.graph.append([w,v1,v2])
 
+    #this is going to use the parent array we create in the kruskals for the returning MST and take in an index(node) in that result tree
+    #res_tree = [0,0,1] -> node 0 is pointed to itself(root), node 1 points to 0 and node 2 points to 1
+    def find(self, res_tree, node):
+        #first get the root of the nodes tree
+        root = node
+        while res_tree[root] != root:
+            root = res_tree[root]
+        #now currNode is the root node for that tree
+        currNode = node
+        #going to point each index to the root index that was in that path
+        while res_tree[currNode] != currNode:
+            currPointer = res_tree[currNode]
+            res_tree[currNode] = root
+            currNode = currPointer
+        return root
+    #union function for adding pointers to the parent array
+    def union(self, res_tree, v1, v2):
+        root1 = self.find(res_tree,v1)
+        root2 = self.find(res_tree,v2)
+        if root1 != root2:
+            res_tree[root2] = root1 #now pointing the root2 array index at the root1 position since not in same tree
+        else:
+            return -1
+        
+    #kruskals alg function: going to take in the graph and create a MST from it which is going to be stored in the res_tree array 
+    #it will contain the index pointers to each nodes root which will be what it is connected to on the graph
+    #each index will hold the pointer and then we need to incremement the total cost each time
+    def kruskals(self):
+        totalV = self.vertices #num of vertices
+        graph = self.graph # this is the graph to work from 
+        res_tree = [i for i in range(totalV)] #create the tree with all nodes pointing at themselves right now
+
+        total_weight = 0 #this will store the total weight gotten from the edges that were successfully added to the tree
+
+        edges_added = 0#this will be the break point-- once added v-1 edges we stop because that is the number to create the tree
+        sortedGraph = sorted(graph, key= lambda edge: edge[0])#sorts the graph by the weight of each edge
+
+        #loop while edges is less than num of vertices because we need one less edge so there are no cycle
+        for weight, v1, v2 in sortedGraph:
+            #try to add the vertices to the result tree by adding connection between
+            if self.union(res_tree, v1,v2) == -1:
+                print(f'Unable to add {v1} and {v2} as a direct connection in the result tree')
+            else:
+                total_weight+=weight
+        for vertex, root in enumerate(res_tree):
+            print(f'{vertex} --> {root}')
+        return (total_weight, res_tree)
+if __name__ == '__main__':
+    # 5-vertex graph, known-good MST:
+    #   0-1(2) 0-3(6) 1-2(3) 1-3(8) 1-4(5) 2-4(7) 3-4(9)
+    # cheapest edges that connect everything without a cycle:
+    #   0-1(2) + 1-2(3) + 1-4(5) + 0-3(6) = 16
+    g = Graph(5)
+    g.addEdge(0, 1, 2)
+    g.addEdge(0, 3, 6)
+    g.addEdge(1, 2, 3)
+    g.addEdge(1, 3, 8)
+    g.addEdge(1, 4, 5)
+    g.addEdge(2, 4, 7)
+    g.addEdge(3, 4, 9)
+
+    total_weight, res_tree = g.kruskals()
+
+    assert total_weight == 16, f'expected MST weight 16, got {total_weight}'
+
+    # every vertex should resolve (via find, not just the raw array value)
+    # to the same root, since the graph is fully connected
+    roots = {g.find(res_tree, v) for v in range(g.vertices)}
+    assert len(roots) == 1, f'expected a single connected tree, got roots {roots}'
+
+    print(f'total weight: {total_weight}')
+    print(f'res_tree: {res_tree}')
+    print('test passed')
+    
+
+
+
+
+        
+
+
+
+
+
+    
